03a33361ac87f080a6fdbb5423e17d0cbfd012a5
Davizera231 <dsandesteixeira54@gmail.com>
2025-10-08 13:19:12 -0300

    feature: adicionando template expression

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import FirstComponent from './components/FirstComponent';
+import TemplateExpression from './components/TemplateExpression';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <TemplateExpression/>
+      <FirstComponent/>
       <h1>Fundamentos React</h1>
     </div>
   );
