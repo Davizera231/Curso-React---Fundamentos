c15cdc4f7bb18a57957f9d95db72f6f29a4b38bb
Davizera231 <dsandesteixeira54@gmail.com>
2025-10-27 15:12:34 -0300

    feature: Adicionando key, para poder extrair os dados da lista por indices

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -4,13 +4,27 @@ import { useState } from "react";
 const ListRender = () => {
 
     const [list] = useState(['Matheus', 'João', 'Maria', 'Pedro']);
+
+    const [users] = useState([
+      { id: 1, name: 'Matheus', age: 30 },
+      { id: 2, name: 'João', age: 25 },
+      { id: 3, name: 'Maria', age: 28 },
+      { id: 4, name: 'Pedro', age: 22 },
+    ]);
   
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
             ))}
+        </ul>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.age}
+              </li>
+          ))}
         </ul>
     </div>
   );
